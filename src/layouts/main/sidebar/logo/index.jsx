afe0b430698d8fb189496b23331de07645b30f70
MelikeDemiralay <melikedemiralay3215@gmail.com>
2024-04-25 04:08:16 +0300

    Added menu component and installed Headless UI

--- src/layouts/main/sidebar/logo/index.jsx
+++ src/layouts/main/sidebar/logo/index.jsx
@@ -4,7 +4,7 @@ const Logo = () => {
     <div className="py-0.5">
       <Link
         to="/"
-        className="text-[color:var(--color-base)] w-[3.25rem] h-[3.25rem] rounded-full flex items-center justify-center hover:bg-[color:var(--background-third)] transition-colors"
+        className="text-[#FFFFFF] w-[3.25rem] h-[3.25rem] rounded-full flex items-center justify-center hover:bg-[#eff3f41a] transition-colors"
       >
         <svg viewBox="0 0 24 24" className="h-[1.875rem]">
           <path
---
+++ src/layouts/main/sidebar/menu/new/index.jsx
@@ -0,0 +1,13 @@
+import Button from "../../../../../components/button";
+
+const New = () => {
+  return (
+    <div>
+      <div className="py-4 w-[90%]">
+        <Button size="large">Gönder</Button>
+      </div>
+    </div>
+  );
+};
+
+export default New;
